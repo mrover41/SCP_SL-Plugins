4867253eada45f2dc066f88bb31abbfb6c9edc03
mrover41 <mrover41@users.noreply.github.com>
2026-01-09 17:38:58 +0200

    remove test code

--- Roles/Loader.cs
+++ Roles/Loader.cs
@@ -7,17 +7,11 @@ namespace Corwarx_Roles {
     public class Loader : Plugin<Config> {
         public override void OnEnabled() {
             RoleManager.RegisterAllRoles(System.Reflection.Assembly.GetExecutingAssembly());
-            Exiled.Events.Handlers.Server.RoundStarted += StartRound;
             base.OnEnabled();
         }
-
-        private void StartRound() {
-            Timing.CallDelayed(1f, () => { RoleManager.AddRole(Player.List.First(), 5); });
-            Log.Info("Starting Round");
-        }
+        
         
         override public void OnDisabled() {
-            Exiled.Events.Handlers.Server.RoundStarted -= StartRound;
             base.OnDisabled();
         }
     }
