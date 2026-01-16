ce1f64fecc26fca5321053364dfa985465a17e86
mrover41 <mrover41@users.noreply.github.com>
2026-01-16 13:48:48 +0200

    config update

--- Corwarx Project/Features/RoleSystem/Containers/RoleConfig.cs
+++ Corwarx Project/Features/RoleSystem/Containers/RoleConfig.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Exiled.CustomItems.API.Features;
 using PlayerRoles;
 
 namespace Corwarx_Project.Features.RoleSystem.Containers {
@@ -7,5 +9,6 @@ namespace Corwarx_Project.Features.RoleSystem.Containers {
         public string Description { get; set; }
         public int ID { get; set; }
         public RoleTypeId RoleTypeId { get; set; }
+        public List<ItemType> Items { get; set; }
     }
 }
